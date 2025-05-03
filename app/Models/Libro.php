a26eae17b86047e5b4a5b528cd9f26d3d5e19577
CF200830 <cu3llar.2002@gmail.com>
2025-05-03 01:27:49 -0600

    Update Libro.php

--- app/Models/Libro.php
+++ app/Models/Libro.php
@@ -8,11 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Libro extends Model
 {
     use HasFactory;
-
+    // Definimos la tabla asociada al modelo
     protected $fillable = ['titulo', 'autor_id'];
-
+    // Definimos la tabla asociada al modelo
     public function autor()
-    {
+    {// Definimos la relación inversa con el modelo Autor
         return $this->belongsTo(Autor::class);
     }
-}
+}}
